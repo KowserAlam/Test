fa9b5fbba3fcb2efd82bcbe7435b4bf0d584def3
Rahat <rahat@ishraak.com>
2020-09-20 17:27:47 +0600

    test folders and login test file created. + fluter driver added

---
+++ test_driver/auth/login_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+
+main(){
+  loginTest();
+}
+Future<void> loginTest()async{
+
+  group('Login Test', () {
+
+    //call key codes will be here
+
+    FlutterDriver driver;
+    // Connect to the Flutter driver before running any tests.
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed.
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    //test cases are started from here
+
+
+  });
+
+}
---
+++ test_driver/keys.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_driver/flutter_driver.dart';
+
+class Keys{
+//Common Keys
+  static final backButton = find.byTooltip('Back');
+}
